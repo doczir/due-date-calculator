811e6e9763470a43df930d708e6b4dd158b61f1d
Robert Doczi <doczi.r@gmail.com>
2017-07-29 12:45:07 +0000

    refactored while loop to a recursive function

--- src/main/scala/datecalc/DueDateCalculator.scala
+++ src/main/scala/datecalc/DueDateCalculator.scala
@@ -3,27 +3,36 @@ package datecalc
 import java.time.temporal.ChronoUnit
 import java.time.{Duration, LocalDateTime, LocalTime}
 
+import scala.annotation.tailrec
+
 object DueDateCalculator {
 
   private val startOfBusinessHours = LocalTime.of(9, 0)
   private val endOfBusinessHours = LocalTime.of(17, 0)
+  private lazy val dailyWork =  Duration.ofMinutes(ChronoUnit.MINUTES.between(startOfBusinessHours, endOfBusinessHours))
 
   def calculateDueDate(submitDate: LocalDateTime, turnaroundTimeHours: Long): LocalDateTime = {
-    var turnaroundTime = Duration.ofHours(turnaroundTimeHours)
+    val turnaroundTime = Duration.ofHours(turnaroundTimeHours)
 
-    var dueDate = submitDate
-    var timeLeftOfDay = Duration.ofMinutes(ChronoUnit.MINUTES.between(dueDate.toLocalTime, endOfBusinessHours))
-
-    while(timeLeftOfDay.getSeconds < turnaroundTime.getSeconds) {
-      dueDate = getNextBusinessDay(dueDate)
-      turnaroundTime = turnaroundTime.minus(timeLeftOfDay)
-      timeLeftOfDay = Duration.ofMinutes(ChronoUnit.MINUTES.between(dueDate.toLocalTime, endOfBusinessHours))
-    }
-
-    dueDate.plus(turnaroundTime)
+    addDailyWork(submitDate, turnaroundTime)
   }
 
   private def getNextBusinessDay(day: LocalDateTime): LocalDateTime = {
     day.plusDays(1).toLocalDate.atTime(startOfBusinessHours)
+  }
+
+  @tailrec
+  def addDailyWork(startOfDay: LocalDateTime, durationLeft: Duration): LocalDateTime = {
+    if (durationLeft.toMinutes == 0) startOfDay
+    else {
+      val timeLeftOfDay = Duration.ofMinutes(ChronoUnit.MINUTES.between(startOfDay.toLocalTime, endOfBusinessHours))
+      if (durationLeft.toMinutes > timeLeftOfDay.toMinutes) {
+        addDailyWork(getNextBusinessDay(startOfDay), durationLeft.minus(timeLeftOfDay))
+      } else if (durationLeft.toMinutes > dailyWork.toMinutes) {
+        addDailyWork(getNextBusinessDay(startOfDay), durationLeft.minus(dailyWork))
+      } else {
+        addDailyWork(startOfDay.plus(durationLeft), durationLeft.minus(durationLeft))
+      }
+    }
   }
 }
