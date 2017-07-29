2cde57b5f10604c6e515ad95a2d3ad68766e585e
Robert Doczi <doczi.r@gmail.com>
2017-07-29 11:34:01 +0000

    due date is same day as submit date test expanded

--- src/test/scala/datecalc/DueDateCalculatorSpec.scala
+++ src/test/scala/datecalc/DueDateCalculatorSpec.scala
@@ -26,6 +26,16 @@ class DueDateCalculatorSpec extends FlatSpec with Matchers {
 
       actual should equal(expected)
     }
+
+    {
+      val submitDate = LocalDateTime.of(2017, 7, 27, 10, 0) // THURSDAY
+      val turnaroundTime = 1
+
+      val expected = LocalDateTime.of(2017, 7, 27, 11, 0) // THURSDAY
+      val actual = DueDateCalculator.calculateDueDate(submitDate, turnaroundTime)
+
+      actual should equal(expected)
+    }
   }
 
 }
