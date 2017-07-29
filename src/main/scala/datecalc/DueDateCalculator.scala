3b12bf572cd56ab97ff998807b220e649500f9d8
Robert Doczi <doczi.r@gmail.com>
2017-07-29 11:55:22 +0000

    implemented skipping after work hours

--- src/main/scala/datecalc/DueDateCalculator.scala
+++ src/main/scala/datecalc/DueDateCalculator.scala
@@ -1,14 +1,27 @@
 package datecalc
 
-import java.time.{Duration, LocalDateTime}
+import java.time.temporal.ChronoUnit
+import java.time.{Duration, LocalDateTime, LocalTime}
 
 
 
 object DueDateCalculator {
 
+  private val startOfBusinessHours = LocalTime.of(9, 0)
+  private val endOfBusinessHours = LocalTime.of(17, 0)
+
   def calculateDueDate(submitDate: LocalDateTime, turnaroundTimeHours: Long): LocalDateTime = {
-    val turnaroundTime = Duration.ofHours(turnaroundTimeHours)
-    submitDate.plus(turnaroundTime)
+    var turnaroundTime = Duration.ofHours(turnaroundTimeHours)
+
+    val timeLeftOfSubmitDay = Duration.ofMinutes(ChronoUnit.MINUTES.between(submitDate.toLocalTime, endOfBusinessHours))
+    var dueDate = submitDate
+
+    if(timeLeftOfSubmitDay.getSeconds < turnaroundTime.getSeconds) {
+      dueDate = submitDate.plusDays(1).toLocalDate.atTime(startOfBusinessHours)
+      turnaroundTime = turnaroundTime.minus(timeLeftOfSubmitDay)
+    }
+
+    dueDate.plus(turnaroundTime)
   }
 
 }
